6de01d346181abc59a7bc102989c41e457b6367b
Hevanafa <dhevansafa4@gmail.com>
2025-12-07 11:40:04 +0700

    added basic SDL texture bindings

--- UNITS/SDL2Wrapper.pas
+++ UNITS/SDL2Wrapper.pas
@@ -9,9 +9,13 @@ const
   SDL_WINDOWPOS_CENTERED = $2FFF0000;
   SDL_WINDOW_SHOWN = $00000004;
 
+  SDL_PIXELFORMAT_RGBA32 = $16462004;
+  SDL_TEXTUREACCESS_STREAMING = 1;
+
 type
   PSDL_Window = pointer;
   PSDL_Renderer = pointer;
+  PSDL_Texture = pointer;
 
 function SDL_Init(const flags: longword): longint; cdecl; external 'SDL2.dll';
 function SDL_CreateWindow(const title: PChar; const x, y, w, h: longint; const flags: longword): PSDL_Window; cdecl; external 'SDL2.dll';
@@ -24,6 +28,11 @@ procedure SDL_Delay(const ms: longword); cdecl; external 'SDL2.dll';
 procedure SDL_SetRenderDrawColor(const renderer: PSDL_Renderer; const r, g, b, a: byte); cdecl; external 'SDL2.dll';
 procedure SDL_RenderClear(const renderer: PSDL_Renderer); cdecl; external 'SDL2.dll';
 procedure SDL_RenderPresent(const renderer: PSDL_Renderer); cdecl; external 'SDL2.dll';
+
+function SDL_CreateTexture(const renderer: PSDL_Renderer; const format: longword; const access, w, h: longint): PSDL_Texture; cdecl; external 'SDL2.dll';
+function SDL_UpdateTexture(texture: PSDL_Texture; rect, pixels: pointer; pitch: longint): longint; cdecl; external 'SDL2.dll';
+function SDL_RenderCopy(renderer: PSDL_Renderer; texture: PSDL_Texture; srcrect, dstrect: pointer): longint; cdecl; external 'SDL2.dll';
+function SDL_DestroyTexture(texture: PSDL_Texture); cdecl; external 'SDL2.dll';
 
 
 implementation
