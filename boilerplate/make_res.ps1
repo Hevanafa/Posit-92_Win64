a66caae11ac7b7df85271c57ab57e817b7f087e0
Hevanafa <dhevansafa4@gmail.com>
2025-12-08 12:48:07 +0700

    added make_res prototype

---
+++ boilerplate/make_res.ps1
@@ -0,0 +1,20 @@
+$resname = "game.res"
+$rcname = "game.rc"
+$iconame = "game.ico"
+$windresPath = "E:\fpc-wasm\fpc\bin\x86_64-win64\windres.exe";
+
+if (test-path $resname) {
+  remove-item $resname
+}
+
+if (!(test-path $rcname)) {
+  write-host "Missing $rcname!" -foregroundColor magenta
+  exit
+}
+
+if (!(test-path $iconame)) {
+  write-host "Missing $iconame!" -ForegroundColor magenta
+  exit
+}
+
+& ($windresPath $rcname -O coff -o $resname)
