22c8982813a9e2e4c772d23680f7718a6aa16880
Hevanafa <dhevansafa4@gmail.com>
2025-12-09 03:56:35 +0700

    copied game.pas from boilerplate to sound demo

--- DEMOS/sound/game.pas
+++ DEMOS/sound/game.pas
@@ -1,117 +1,101 @@
-library Game;
+{$R game.res}
 
-{$Mode ObjFPC}
+{$Mode TP}
 
 uses
-  Conv, FPS, Graphics,
-  ImgRef, ImgRefFast, Keyboard, Logger, Mouse,
-  Panic, Sounds, Timing, VGA,
+  SDL2Wrapper, Posit92,
+  Keyboard, Mouse,
+  Logger, ImgRef, ImgRefFast,
+  Sounds, Timing, VGA,
   Assets;
 
 const
-  SC_ESC = $01;
-  SC_SPACE = $39;
-
-  SC_1 = $02;
-  SC_2 = $03;
-  SC_3 = $04;
-  SC_4 = $05;
-  SC_5 = $06;
+  TargetFPS = 60;
+  FrameTime = 16;
 
 var
-  lastEsc, lastSpacebar: boolean;
-  lastD1, lastD2, lastD3, lastD4, lastD5: boolean;
-
-  { Init your game state here }
   gameTime: double;
+  { More of your game state here }
 
-{ Use this to set `done` to true }
-procedure signalDone; external 'env' name 'signalDone';
 
-procedure drawFPS;
-begin
-  printDefault('FPS:' + i32str(getLastFPS), 240, 0);
-end;
+type
+  TGame = object(TPosit92)
+  public
+    procedure loadAssets;
+  private
+    procedure init;
+    procedure afterInit;
+    procedure cleanup;
+    procedure update;
+    procedure draw;
+  end;
 
 procedure drawMouse;
 begin
   spr(imgCursor, mouseX, mouseY)
 end;
 
-procedure playRandomSFX;
+
+procedure TGame.loadAssets;
 begin
-  playSound(1 + random(SfxSlip))
+  imgCursor := loadImage('assets\images\cursor.png');
+  imgDosuEXE[0] := loadImage('assets\images\dosu_1.png');
+  imgDosuEXE[1] := loadImage('assets\images\dosu_2.png');
+
+  loadBMFont(
+    'assets\fonts\nokia_cellphone_fc_8.txt',
+    defaultFont, defaultFontGlyphs)
+
+  { Load more assets here }
 end;
 
-
-procedure init;
+procedure TGame.init;
 begin
+  inherited init; { works the same as super.init() in JS }
+
+  setTitle('Posit-92 with SDL2');
+
+  initLogger;
   initBuffer;
   initDeltaTime;
-  initFPSCounter;
+  initSounds;
 end;
 
-procedure afterInit;
+procedure TGame.afterInit;
 begin
-  { Initialise game state here }
+  loadAssets;
   hideCursor;
+
+  { Init your game state here }
+  gameTime := 0.0
 end;
 
-procedure update;
+procedure TGame.cleanup;
 begin
-  updateDeltaTime;
-  incrementFPS;
+  closeLogger;
+  showCursor;
+  cleanupSounds;
 
-  updateMouse;
+  { Your cleanup code here (after setting `done` to true) }
+
+  inherited cleanup;
+end;
+
+procedure TGame.update;
+begin
+  inherited update;
+  updateDeltaTime;
 
   { Your update logic here }
-  if lastEsc <> isKeyDown(SC_ESC) then begin
-    lastEsc := isKeyDown(SC_ESC);
-
-    if lastEsc then begin
-      writeLog('ESC is pressed!');
-      signalDone
-    end;
-  end;
-
-  if lastSpacebar <> isKeyDown(SC_SPACE) then begin
-    lastSpacebar := isKeyDown(SC_SPACE);
-
-    if lastSpacebar then playRandomSFX;
-  end;
-
-  if lastD1 <> isKeyDown(SC_1) then begin
-    lastD1 := isKeyDown(SC_1);
-    if lastD1 then playSound(1);
-  end;
-
-  if lastD2 <> isKeyDown(SC_2) then begin
-    lastD2 := isKeyDown(SC_2);
-    if lastD2 then playSound(2);
-  end;
-
-  if lastD3 <> isKeyDown(SC_3) then begin
-    lastD3 := isKeyDown(SC_3);
-    if lastD3 then playSound(3);
-  end;
-
-  if lastD4 <> isKeyDown(SC_4) then begin
-    lastD4 := isKeyDown(SC_4);
-    if lastD4 then playSound(4);
-  end;
-
-  if lastD5 <> isKeyDown(SC_5) then begin
-    lastD5 := isKeyDown(SC_5);
-    if lastD5 then playSound(5);
-  end;
+  if isKeyDown(SC_ESC) then done := true;
 
   gameTime := gameTime + dt
 end;
 
-procedure draw;
+procedure TGame.draw;
 var
-  w: integer;
   s: string;
+  w: word;
 begin
   cls($FF6495ED);
 
@@ -120,28 +104,39 @@ begin
   else
     spr(imgDosuEXE[0], 148, 88);
 
-  s := '1, 2, 3, 4, 5 - Play sound';
+  s := 'Hello world!';
   w := measureDefault(s);
   printDefault(s, (vgaWidth - w) div 2, 120);
 
-  s := 'Spacebar - Play a random sound';
-  w := measureDefault(s);
-  printDefault(s, (vgaWidth - w) div 2, 130);
-
   drawMouse;
-  drawFPS;
-
   vgaFlush
 end;
 
-exports
-  { Main game procedures }
-  init,
-  afterInit,
-  update,
-  draw;
+
+var
+  game: TGame;
+  lastFrameTime, frameTimeNow, elapsed: longword; { in ms }
 
 begin
-{ Starting point is intentionally left empty }
-end.
+  game.init;
+  game.afterInit;
 
+  game.done := false;
+
+  lastFrameTime := SDL_GetTicks;
+
+  while not game.done do begin
+    frameTimeNow := SDL_GetTicks;
+    elapsed := frameTimeNow - lastFrameTime;
+
+    if elapsed >= FrameTime then begin
+      lastFrameTime := frameTimeNow - (elapsed mod FrameTime); { Carry over extra time }
+      game.update;
+      game.draw
+    end;
+
+    SDL_Delay(1)
+  end;
+
+  game.cleanup
+end.
