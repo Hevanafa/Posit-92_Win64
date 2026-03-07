22a29ec5bf29717668689b54bd81fcb07e6f8af2
Hevanafa <dhevansafa4@gmail.com>
2026-03-08 00:24:53 +0700

    updated measure BMFont uses

--- DEMOS/sound/assets.pas
+++ DEMOS/sound/assets.pas
@@ -37,7 +37,7 @@ end;
 
 function measureDefault(const text: string): word;
 begin
-  measureDefault := measureBMFont(defaultFontGlyphs, text)
+  measureDefault := measureBMFont(defaultFont, defaultFontGlyphs, text)
 end;
 
 
