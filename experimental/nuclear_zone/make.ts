613d8c47595dad162815c94fa5a1c3030deed351
Hevanafa <dhevansafa4@gmail.com>
2026-03-15 06:56:34 +0700

    added make script text output

--- experimental/nuclear_zone/make.ts
+++ experimental/nuclear_zone/make.ts
@@ -2,6 +2,7 @@ import Bun from "bun";
 import { existsSync } from "node:fs";
 import path from "node:path";
 import { stderr, stdout } from "node:process";
+import { styleText } from "node:util";
 
 const compilerPath = "E:\\fpc-wasm\\fpc\\bin\\x86_64-win64\\fpc.exe";
 const primaryUnit = ".\\game.pas";
@@ -24,9 +25,22 @@ const unitPaths = [
   "shared"
 ];
 
-const result = spawnSync(
+const result = Bun.spawnSync(
   [compilerPath, "-Twin64", ...unitPaths.map(path => "-Fu" + path), `-o${outputFile}`, primaryUnit],
   { cwd: scriptDir, stdout: "pipe", stderr: "pipe" }
 );
 
 // TODO: Handle stdout and stderr
+
+console.log(styleText("cyan", "(STDOUT)"));
+console.log(stdout == "" ? styleText("gray", "(No data)") : stdout);
+
+console.log(styleText("red", "(STDERR)"));
+console.log(stderr == "" ? styleText("gray", "(No data)") : stdout);
+
+const exitCode = result.exitCode ?? 1;
+
+if (exitCode != 0) {
+  console.log(styleText("red", `Compilation failed with exit code ${exitCode}`));
+  process.exit(exitCode)
+}
