ae549fb36e99cf0d5096c53aafc1c8efd22dc253
Hevanafa <dhevansafa4@gmail.com>
2026-03-15 08:00:13 +0700

    finalised dist script

--- experimental/nuclear_zone/dist.ts
+++ experimental/nuclear_zone/dist.ts
@@ -1,14 +1,19 @@
-import { copyFileSync, existsSync, mkdirSync } from "node:fs";
+import { copyFileSync, cpSync, existsSync, mkdirSync } from "node:fs";
 import path from "node:path";
 import { styleText } from "node:util";
 
 const scriptDir = import.meta.dir;
 const distDir = path.join(scriptDir, "dist");
 
-const DLLs = ["SDL2.dll", "SDL_image.dll", "SDL_mixer.dll"];
+const MainExe = "game.exe";
+const DLLs = ["SDL2.dll", "SDL2_image.dll", "SDL2_mixer.dll"];
+const AssetsDir = "assets";
 
 if (!existsSync(distDir))
   mkdirSync(distDir);
+
+cpSync(MainExe, path.join(distDir, MainExe));
+console.log(styleText("cyan", "Copied " + MainExe));
 
 for (const dll of DLLs) {
   const src = path.join(scriptDir, dll);
@@ -22,4 +27,13 @@ for (const dll of DLLs) {
   console.log(styleText("cyan", `Copied ${dll}`))
 }
 
-// TODO: Handle assets copying
+// Handle assets
+const assetsSrc = path.join(scriptDir, AssetsDir);
+
+if (!existsSync(assetsSrc)) {
+  console.log(styleText("red", `Missing ${AssetsDir}!`));
+  process.exit(1)
+} else {
+  cpSync(assetsSrc, path.join(distDir, AssetsDir), { recursive: true });
+  console.log(styleText("cyan", `Copied ${AssetsDir}`))
+}
