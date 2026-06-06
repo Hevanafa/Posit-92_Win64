167c33c2828357c1022813847ca67505db288e2a
Hevanafa <dhevansafa4@gmail.com>
2026-06-07 06:14:14 +0700

    fixed changed filenames in setup script

--- boilerplate/setup.ps1
+++ boilerplate/setup.ps1
@@ -1,6 +1,6 @@
 # Setup Boilerplate
-# By Hevanafa, 08-12-2025
-# Part of Posit-92 framework
+# By Hevanafa
+# Part of Posit-92 game engine
 # Modified from the Wasm version
 
 # This script should be executed before copying as a new demo
@@ -19,7 +19,7 @@ copy-item "$source\DLL\*.dll" ".\"
 copy-item "$source\shared\*.pas" ".\shared\"
 copy-item "$source\UNITS\*.pas" ".\UNITS\"
 
-$scripts = @("build_run", "cleanup", "compile", "make_res", "run")
+$scripts = @("build_run", "clean", "make", "make_res", "run")
 foreach ($script in $scripts) {
   copy-item "$source\scripts\$script.ps1" ".\"
 }
