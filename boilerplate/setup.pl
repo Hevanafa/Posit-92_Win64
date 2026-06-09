2faed69cd628e756b90a2066b4a584ba95849ebf
Hevanafa <dhevansafa4@gmail.com>
2026-06-09 12:07:21 +0700

    refactored copy demo procedure

--- boilerplate/setup.pl
+++ boilerplate/setup.pl
@@ -15,38 +15,36 @@ my $dest = abs_path(".");
 
 # Copy from the demo folder
 
-# Quirk: `find` calls `chdir` into each directory as it "walks",
-# since it's essentially a "walker", so using abs_path is necessary
+sub copy_demo {
+  # Quirk: `find` calls `chdir` into each directory as it "walks",
+  # since it's essentially a "walker", so using abs_path is necessary
 
-find(sub {
-  return unless -f;
+  find(sub {
+    return unless -f;
 
-  my $filename = $_;
-  my $src_file = $File::Find::name;
-  # my $dirname = $File::Find::dir;
+    my $filename = $_;
+    my $src_file = $File::Find::name;
+    # my $dirname = $File::Find::dir;
 
-  (my $relative = $src_file) =~ s/^\Q$src\E//;
+    (my $relative = $src_file) =~ s/^\Q$src\E//;
 
-  say "Relative path: ".$relative;
+    # say "Relative path: ".$relative;
 
-  my @chunks = (split /\//, "$dest$relative");
-  @chunks = @chunks[0..$#chunks - 1];
-  my $target_dir = (join "/", @chunks);
+    my @chunks = (split /\//, "$dest$relative");
+    @chunks = @chunks[0..$#chunks - 1];
+    my $target_dir = (join "/", @chunks);
 
-  say "Target dir  : ".$target_dir;
+    # say "Target dir  : ".$target_dir;
 
-  make_path($target_dir);
+    make_path($target_dir);
 
-  if (-e $src_file) {
-    say "File exists!"
-  } else {
-    say "File doesn't exist!"
-  }
+    copy($src_file, $target_dir."/".$filename)
+      or warn "Failed to copy $File::Find::name $!";
 
-  copy($src_file, $target_dir."/".$filename)
-    or warn "Failed to copy $File::Find::name $!";
+    # say "";
+  }, $src);
+}
 
-  say "";
-}, $src);
+copy_demo;
 
 print "Setup complete!"
