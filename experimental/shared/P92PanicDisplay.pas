cfba540a8a2c3a1540f3a05f5778d54aeea41aa0
Hevanafa <dhevansafa4@gmail.com>
2026-07-11 09:33:39 +0700

    copied sounds and panic display units

---
+++ experimental/shared/P92PanicDisplay.pas
@@ -0,0 +1,57 @@
+unit P92PanicDisplay;
+
+{$Mode ObjFPC}
+{$H-}{$J-}
+
+interface
+
+procedure PanicHaltDisplay(const msg: string);
+procedure PascalPanicHaltDisplay; public name 'PascalPanicHaltDisplay';
+
+
+implementation
+
+uses
+  P92Core, P92InteropBuf, P92TexDraw, P92Panic, P92VGA
+{$ifdef P92_WEBGL}
+  , P92WebGL
+{$endif}
+  ;
+
+procedure PanicHaltDisplay(const msg: string);
+var
+  a, b: word;
+begin
+  cls($FF550000);
+
+  { Scanlines }
+  b:=0;
+  while b < VgaHeight do begin
+    for a:=0 to VgaWidth - 1 do
+      unsafePset(a, b, $FF2A0000);
+
+    inc(b, 3)
+  end;
+
+  Print('Fatal Error', 8, 8);
+
+  PrintWrap(msg, 8, 24, VgaWidth - 16);
+
+  Print('Check Console for details', 8, VgaHeight - 16);
+
+  VgaUpload;
+{$ifdef P92_WEBGL}
+  WebGLPresent;
+{$else}
+  VgaPresent;
+{$endif}
+
+  PanicHalt(msg)
+end;
+
+procedure PascalPanicHaltDisplay;
+begin
+  PanicHaltDisplay(ReadInteropString)
+end;
+
+end.
